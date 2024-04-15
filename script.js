cc857751e2b86e76bd09528f7a6197cc9fe04e91
Cristiano Silva <crizsil@gmail.com>
2024-04-14 23:10:04 -0300

    adicionando temporizador

--- script.js
+++ script.js
@@ -5,11 +5,19 @@ const longoBt = document.querySelector('.app__card-button--longo')
 const banner = document.querySelector('.app__image')
 const titulo = document.querySelector('.app__title')
 const botoes = document.querySelectorAll('.app__card-button')
+const startPauseBt = document.querySelector('#start-pause')
 const musicaFocoInput = document.querySelector('#alternar-musica')
 const musica = new Audio('/sons/luna-rise-part-one.mp3')
+const audioPlay = new Audio('/sons/play.wav');
+const audioPausa = new Audio('/sons/pause.mp3');
+const audioTempoFinalizado = new Audio('./sons/beep.mp3')
+
+let tempoDecorridoEmSegundos = 5
+let intervaloId = null
+
 musica.loop = true
 
-musicaFocoInput.addEventListener('change', () =>{
+musicaFocoInput.addEventListener('change', () => {
     if(musica.paused) {
         musica.play()
     } else {
@@ -33,28 +41,56 @@ longoBt.addEventListener('click', () => {
 })
 
 function alterarContexto(contexto) {
-    botoes.forEach(function (contexto) {
+    botoes.forEach(function (contexto){
         contexto.classList.remove('active')
     })
-    html.setAttribute('data-contexto',contexto)
-    banner.setAttribute('src',`/imagens/${contexto}.png`)
-    switch(contexto) {
-        case "foco" :
-            titulo.innerHTML = `Otimize sua produtividade,<br>
-            <strong class="app__title-strong">mergulhe no que importa.</strong>
-        `    
-        break;
-        case 'descanso-curto' :
-            titulo.innerHTML = `Que tal dar uma respirada,<br>
-            <strong class="app__title-strong">Faça uma pausa curta.</strong>
-        `
-        break;
-        case 'descanso-longo' : 
-            titulo.innerHTML = `Hora de voltar à superficie,<br>
-            <strong class="app__title-strong">Faça uma pausa longa.</strong>
-        `
+    html.setAttribute('data-contexto', contexto)
+    banner.setAttribute('src', `/imagens/${contexto}.png`)
+    switch (contexto) {
+        case "foco":
+            titulo.innerHTML = `
+            Otimize sua produtividade,<br>
+                <strong class="app__title-strong">mergulhe no que importa.</strong>
+            `
+            break;
+        case "descanso-curto":
+            titulo.innerHTML = `
+            Que tal dar uma respirada? <strong class="app__title-strong">Faça uma pausa curta!</strong>
+            ` 
+            break;
+        case "descanso-longo":
+            titulo.innerHTML = `
+            Hora de voltar à superfície.<strong class="app__title-strong"> Faça uma pausa longa.</strong>
+            `
         default:
             break;
     }
+}
+
+const contagemRegressiva = () => {
+    if(tempoDecorridoEmSegundos <= 0){
+        zerar()
+        alert('Tempo finalizado!')
+        return
+    }
+    tempoDecorridoEmSegundos -= 1
+    console.log('Temporizador: ' + tempoDecorridoEmSegundos)
+}
+
+startPauseBt.addEventListener('click', iniciarOuPausar)
+
+function iniciarOuPausar() {
+    if(intervaloId){
+        audioPausa.play()
+        zerar()
+        return
+    }
+    audioPlay.play()
+    intervaloId = setInterval(contagemRegressiva, 1000)
+}
+
+function zerar() {
+    clearInterval(intervaloId) 
+    intervaloId = null
 }
 
